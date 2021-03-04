ea476480d636f86967c9e36dfd9c336166beb62c
Cassie Schultz <schu4276@morris.umn.edu>
2021-03-04 16:36:57 -0600

    Generate wordlist component
    This generates the wordlist component and updates the app.module.ts file to import the Wordlist component

---
+++ client/src/app/wordlists/wordlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wordlist',
+  templateUrl: './wordlist.component.html',
+  styleUrls: ['./wordlist.component.scss']
+})
+export class WordlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
