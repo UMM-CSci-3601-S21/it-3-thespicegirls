756d8ba20b2162c4d22ef213fca2e0a17c1b60fc
Cassie <schu4276@morris.umn.edu>
2021-03-10 15:16:08 -0600

    Test add nouns
    This tests that when called the add nouns function will add an array of nouns to the context pack object

--- client/src/app/contextpacks/add-contextpacks.component.spec.ts
+++ client/src/app/contextpacks/add-contextpacks.component.spec.ts
@@ -1,5 +1,6 @@
+import { compileComponentFromMetadata } from '@angular/compiler';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule, FormGroup, AbstractControl } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormGroup, AbstractControl, FormArray } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -103,6 +104,20 @@ describe('AddContextpacksComponent', () => {
       component.addWordlist();
       formValue = component.contextPackForm.value;
       expect(formValue.wordlists.length).toBeGreaterThan(2);
+    });
+  });
+  describe('Add nouns', () =>{
+    it('should add an array of nouns when prompted', () =>{
+
+      component.addWordlist();
+      component.addPosArray(0, 'nouns');
+      let control = ((component.contextPackForm.value.wordlists as Array<any>)[0]);
+      console.log(control.nouns);
+      expect(control.nouns.length).toBeGreaterThan(1);
+      // Add 2 noun arrays, we expect two to be present
+      component.addPosArray(0, 'nouns');
+      control = ((component.contextPackForm.value.wordlists as Array<any>)[0]);
+      expect(control.nouns.length).toBeGreaterThan(2);
     });
   });
 
