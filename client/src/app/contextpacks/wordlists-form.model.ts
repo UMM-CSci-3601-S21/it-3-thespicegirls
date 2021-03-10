7c6e336a8185f941a7f83709e4482360d28a046e
Cassie <schu4276@morris.umn.edu>
2021-03-10 13:48:31 -0600

    Remove unneeded files
    These files are no longer needed, as I went a different direction with what I was doing.

--- client/src/app/contextpacks/wordlists-form.model.ts
+++
@@ -1,9 +0,0 @@
-import { FormArray, FormControl, Validators } from '@angular/forms';
-import { Wordlist } from './contextpack';
-
-export class WordlistForm {
-  name = new FormControl();
-  enabled = new FormControl();
-  nouns = new FormArray([]);
-
-}
