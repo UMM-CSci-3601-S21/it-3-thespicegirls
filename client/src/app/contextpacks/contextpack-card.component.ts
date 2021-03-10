559c678bc2d2e2fac714067d72f8da73554dab66
Thomas <dahlg136@morris.umn.edu>
2021-03-09 20:51:54 -0600

    Remove unused imports
    Removed these to make LGTM happy  and to also clean up code

--- client/src/app/contextpacks/contextpack-card.component.ts
+++ client/src/app/contextpacks/contextpack-card.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { of } from 'rxjs';
 import { ContextPack } from './contextpack';
 
 @Component({
--- client/src/app/contextpacks/contextpack.service.ts
+++ client/src/app/contextpacks/contextpack.service.ts
@@ -2,8 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { ContextPack, Wordlist, Word } from './contextpack';
-import { map } from 'rxjs/operators';
+import { ContextPack } from './contextpack';
 
 @Injectable()
 export class ContextPackService {
