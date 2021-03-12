bdaefba81ec960431e309e8e1e39809f1a244d4e
Thomas <dahlg136@morris.umn.edu>
2021-03-11 19:50:35 -0600

    Fix display wordlist tests to have better coverage
    The tests were good, but they didn't cover all scenarios of the actual implemented code.

--- client/src/app/contextpacks/contextpack-card.component.spec.ts
+++ client/src/app/contextpacks/contextpack-card.component.spec.ts
@@ -2,7 +2,8 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { ContextPackCardComponent } from './contextpack-card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
-import { Word, Wordlist, ContextPack } from './contextpack';
+import { Word, Wordlist } from './contextpack';
+
 
 describe('ContextPackCardComponent', () => {
 
@@ -10,6 +11,8 @@ describe('ContextPackCardComponent', () => {
   let fixture: ComponentFixture<ContextPackCardComponent>;
   let component2: ContextPackCardComponent;
   let fixture2: ComponentFixture<ContextPackCardComponent>;
+  let component3: ContextPackCardComponent;
+  let fixture3: ComponentFixture<ContextPackCardComponent>;
   let wordlist: Wordlist;
 
   beforeEach(waitForAsync(() => {
@@ -26,9 +29,12 @@ describe('ContextPackCardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ContextPackCardComponent);
     fixture2 = TestBed.createComponent(ContextPackCardComponent);
+    fixture3 = TestBed.createComponent(ContextPackCardComponent);
+
 
     component = fixture.componentInstance;
     component2 = fixture2.componentInstance;
+    component3 = fixture3.componentInstance;
 
     const noun: Word = {
       word: 'you',
@@ -58,6 +64,22 @@ describe('ContextPackCardComponent', () => {
       adjectives: testAdjectives,
       misc: testMisc
     }];
+    const testWordListBig: Wordlist[] = [{
+      name: 'howdy',
+      enabled: true,
+      nouns: testNouns,
+      verbs: testVerbs,
+      adjectives: testAdjectives,
+      misc: testMisc
+    },
+  {
+      name: 'partner',
+      enabled: true,
+      nouns: testNouns,
+      verbs: testVerbs,
+      adjectives: testAdjectives,
+      misc: testMisc
+  }];
     wordlist = {
 
     };
@@ -69,9 +91,15 @@ describe('ContextPackCardComponent', () => {
       wordlists: testWordList
     };
     component2.contextpack = {
-      _id: 'pat_id',
+      _id: 'mat_id',
       enabled: true,
-      name: 'happy',
+      name: 'Joy',
+    };
+    component3.contextpack = {
+      _id: 'kat_id',
+      enabled: true,
+      name: 'Franz',
+      wordlists: testWordListBig
     };
 
     fixture.detectChanges();
@@ -93,10 +121,10 @@ describe('ContextPackCardComponent', () => {
     expect(component.displayAllNouns(component.contextpack)).toContain('you, yoyo, yos, yoted');
   });
   it('should return the verbs when displayAllVerbs() is called', () => {
-    expect(component.displayAllVerbs(component.contextpack)).toContain('green, greener');
+    expect(component.displayAllVerbs(component.contextpack)).toContain('ran, running');
   });
   it('should return the adjectives when displayAllAdjectives() is called', () => {
-    expect(component.displayAllAdjectives(component.contextpack)).toContain('ran, running');
+    expect(component.displayAllAdjectives(component.contextpack)).toContain('green, greener');
   });
   it('should return the misc words when displayAllMisc() is called', () => {
     expect(component.displayAllMisc(component.contextpack)).toContain('langerhans, langerhan');
@@ -108,6 +136,13 @@ describe('ContextPackCardComponent', () => {
     expect(component.displayVerbs(wordlist)).toBeNull();
     expect(component.displayAdjectives(wordlist)).toBeNull();
     expect(component.displayMisc(wordlist)).toBeNull();
+  });
+
+  it('should have displayNouns,ver,adjective,misc return null if undefined', () => {
+    expect(component2.displayAllNouns(component2.contextpack)).toBeNull();
+    expect(component2.displayAllVerbs(component2.contextpack)).toBeNull();
+    expect(component2.displayAllAdjectives(component2.contextpack)).toBeNull();
+    expect(component2.displayAllMisc(component2.contextpack)).toBeNull();
   });
 
   /**
