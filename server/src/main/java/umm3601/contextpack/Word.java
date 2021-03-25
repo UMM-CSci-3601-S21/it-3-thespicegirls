61da4e04876be401fc4d6e2694c04aa8b303aaea
Cassie Schultz <schu4276@morris.umn.edu>
2021-03-25 15:04:57 -0500

    Add getter and setter methods for Word object
    This adds a couple of getter and setter methods for the word object.  Specifically the getting and setting of forms. These are being used to edit word lists within context packs

--- server/src/main/java/umm3601/contextpack/Word.java
+++ server/src/main/java/umm3601/contextpack/Word.java
@@ -9,5 +9,17 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public class Word {
   public String word;
   public ArrayList<String> forms;
+
+  public ArrayList<String> getForms(){
+    return this.forms;
+  }
+
+  public void setForms(ArrayList<String> forms){
+    this.forms = forms;
+  }
+  public void addForm(String form){
+    this.forms.add(form);
+
+  }
 }
 
