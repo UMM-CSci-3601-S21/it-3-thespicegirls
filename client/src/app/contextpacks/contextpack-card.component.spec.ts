7afe55d5316140b50425de92f1c730414170a19d
Cassie <schu4276@morris.umn.edu>
2021-03-29 16:53:19 -0500

    Remove tests for old functions
    We changed the way words are displayed, now they are displayed as chips without all of the forms.  This means that the old functions we used to display words is no longer used.  As a result the tests for these functions are unnecessary and cause things to fail.

--- client/src/app/contextpacks/contextpack-card.component.spec.ts
+++ client/src/app/contextpacks/contextpack-card.component.spec.ts
@@ -5,7 +5,8 @@ import { MatCardModule } from '@angular/material/card';
 import { Word, Wordlist } from './contextpack';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { ContextPackService } from './contextpack.service';
-import { MockContextPackService } from 'src/testing/contextpack.service.mock';
+import { MockContextPackService } from 'src/testing/contextpack.service.mock';3
+import {MatChipsModule} from '@angular/material/chips';
 
 
 describe('ContextPackCardComponent', () => {
@@ -97,26 +98,7 @@ describe('ContextPackCardComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should list the nouns, verbs, adjectives and misc words when displayWordlist() is called', () => {
-    expect(component.displayWordlists(component.contextpack.wordlists[0])).toContain('you, yoyo, yos, yoted');
-    expect(component.displayWordlists(component.contextpack.wordlists[0])).toContain('green, greener');
-    expect(component.displayWordlists(component.contextpack.wordlists[0])).toContain('ran, running');
-    expect(component.displayWordlists(component.contextpack.wordlists[0])).toContain('langerhans, langerhan');
-    expect(component.displayWordlists(component.contextpack.wordlists[0])).not.toContain('barbie');
-  });
 
-  it('should return the nouns displayAllNouns() is called', () => {
-    expect(component.displayAllWords(component.contextpack, 'nouns')).toContain('you, yoyo, yos, yoted, you, yoyo, yos, yoted');
-  });
-  it('should return the verbs when displayAllVerbs() is called', () => {
-    expect(component.displayAllWords(component.contextpack, 'verbs')).toContain('ran, running, ran, running');
-  });
-  it('should return the adjectives when displayAllAdjectives() is called', () => {
-    expect(component.displayAllWords(component.contextpack, 'adjectives')).toContain('green, greener, green, greener');
-  });
-  it('should return the misc words when displayAllMisc() is called', () => {
-    expect(component.displayAllWords(component.contextpack, 'misc')).toContain('langerhans, langerhan, langerhans, langerhan');
-  });
 
 
   it('should have displayNouns,verbs,adjective,misc return null if undefined', () => {
@@ -126,12 +108,7 @@ describe('ContextPackCardComponent', () => {
     expect(component.displayWords(emptyWordlist, 'misc')).toBeNull();
   });
 
-  it('should have displayNouns,verbs,adjective,misc return null if undefined', () => {
-    expect(component2.displayAllWords(component2.contextpack, 'nouns')).toBeNull();
-    expect(component2.displayAllWords(component2.contextpack, 'verbs')).toBeNull();
-    expect(component2.displayAllWords(component2.contextpack, 'adjectives')).toBeNull();
-    expect(component2.displayAllWords(component2.contextpack, 'misc')).toBeNull();
-  });
+
 
   it('should create a download element when given a json', () => {
     expect(component.downloadJson(component.contextpack, component.contextpack.name).toString()).toContain('happy');
