53b437d4b3d7d79605dd02d8eb5e3096fe82c494
Thomas <dahlg136@morris.umn.edu>
2021-04-01 19:59:33 -0500

    Add method to check if user is already in the database
    Instead of adding a user to the database on every login it now checks if you are already in it.

--- server/src/main/java/umm3601/user/UserController.java
+++ server/src/main/java/umm3601/user/UserController.java
@@ -1,18 +1,11 @@
 package umm3601.user;
 
-import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
-import static com.mongodb.client.model.Filters.regex;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
+
 import java.util.Collections;
-import java.util.List;
-import java.util.regex.Pattern;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
@@ -21,28 +14,18 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.common.collect.ImmutableMap;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Sorts;
 
-import org.bson.Document;
-import org.bson.conversions.Bson;
-import org.bson.types.ObjectId;
 import org.mongojack.JacksonMongoCollection;
 
 import io.javalin.http.BadRequestResponse;
 import io.javalin.http.Context;
-import io.javalin.http.NotFoundResponse;
+
 
 
 /**
  * Controller that manages requests for info about users.
  */
 public class UserController {
-
-  private static final String AGE_KEY = "age";
-  private static final String COMPANY_KEY = "company";
-  private static final String ROLE_KEY = "role";
-
-  static String emailRegex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
 
   private final JacksonMongoCollection<User> userCollection;
 
@@ -59,62 +42,22 @@ public class UserController {
    * Get the single user specified by the `id` parameter in the request.
    *
    * @param ctx a Javalin HTTP context
+   * @return
    */
-  public void getUser(Context ctx) {
-    String id = ctx.pathParam("id");
+  public boolean getUser(String sub) {
+    String id = sub;
     User user;
-
-    try {
-      user = userCollection.find(eq("_id", new ObjectId(id))).first();
-    } catch(IllegalArgumentException e) {
-      throw new BadRequestResponse("The requested user id wasn't a legal Mongo Object ID.");
-    }
+    boolean exists;
+    user = userCollection.find(eq("sub", id)).first();
     if (user == null) {
-      throw new NotFoundResponse("The requested user was not found");
-    } else {
-      ctx.json(user);
+      exists = false;
     }
+    else {
+      exists = true;
+    }
+    return exists;
   }
 
-  /**
-   * Delete the user specified by the `id` parameter in the request.
-   *
-   * @param ctx a Javalin HTTP context
-   */
-  public void deleteUser(Context ctx) {
-    String id = ctx.pathParam("id");
-    userCollection.deleteOne(eq("_id", new ObjectId(id)));
-  }
-
-  /**
-   * Get a JSON response with a list of all the users.
-   *
-   * @param ctx a Javalin HTTP context
-   */
-  public void getUsers(Context ctx) {
-
-    List<Bson> filters = new ArrayList<>(); // start with a blank document
-
-    if (ctx.queryParamMap().containsKey(AGE_KEY)) {
-        int targetAge = ctx.queryParam(AGE_KEY, Integer.class).get();
-        filters.add(eq(AGE_KEY, targetAge));
-    }
-
-    if (ctx.queryParamMap().containsKey(COMPANY_KEY)) {
-      filters.add(regex(COMPANY_KEY,  Pattern.quote(ctx.queryParam(COMPANY_KEY)), "i"));
-    }
-
-    if (ctx.queryParamMap().containsKey(ROLE_KEY)) {
-      filters.add(eq(ROLE_KEY, ctx.queryParam(ROLE_KEY)));
-    }
-
-    String sortBy = ctx.queryParam("sortby", "name"); //Sort by sort query param, default is name
-    String sortOrder = ctx.queryParam("sortorder", "asc");
-
-    ctx.json(userCollection.find(filters.isEmpty() ? new Document() : and(filters))
-      .sort(sortOrder.equals("desc") ?  Sorts.descending(sortBy) : Sorts.ascending(sortBy))
-      .into(new ArrayList<>()));
-  }
 
   /**
    * Get a JSON response with a list of all the users.
@@ -123,29 +66,9 @@ public class UserController {
    * @return
    */
   public String addNewUser(User user) {
-
     userCollection.insertOne(user);
     return user._id;
-
   }
-
-  /**
-   * Utility function to generate the md5 hash for a given string
-   *
-   * @param str the string to generate a md5 for
-   */
-  @SuppressWarnings("lgtm[java/weak-cryptographic-algorithm]")
-  public String md5(String str) throws NoSuchAlgorithmException {
-    MessageDigest md = MessageDigest.getInstance("MD5");
-    byte[] hashInBytes = md.digest(str.toLowerCase().getBytes(StandardCharsets.UTF_8));
-
-    StringBuilder result = new StringBuilder();
-    for (byte b : hashInBytes) {
-      result.append(String.format("%02x", b));
-    }
-    return result.toString();
-  }
-
 
   GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
     // Specify the CLIENT_ID of the app that accesses the backend:
@@ -166,31 +89,28 @@ public void checkToken(Context ctx) throws GeneralSecurityException, IOException
     String userId = payload.getSubject();
     System.out.println("User ID: " + userId);
 
-    // Get profile information from payload
-    String email = payload.getEmail();
-    boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
-    String name = (String) payload.get("name");
-    String pictureUrl = (String) payload.get("picture");
-    String locale = (String) payload.get("locale");
-    String familyName = (String) payload.get("family_name");
-    String givenName = (String) payload.get("given_name");
-    String sub = (String) payload.get("sub");
-    User user = new User();
-    user.email = email;
-    user.emailVerified = emailVerified;
-    user.name = name;
-    user.pictureUrl = pictureUrl;
-    user.locale = locale;
-    user.familyName = familyName;
-    user.givenName = givenName;
-    user.sub = sub;
+    if (getUser(payload.get("sub").toString())){
+      ctx.status(201);
+      ctx.json(ImmutableMap.of("id", true));
+    }
+    else{
+      User user = new User();
+      user.email = payload.getEmail();
+      user.emailVerified = Boolean.valueOf(payload.getEmailVerified());
+      user.name = (String) payload.get("name");
+      user.pictureUrl = (String) payload.get("picture");
+      user.locale = (String) payload.get("locale");
+      user.familyName = (String) payload.get("family_name");
+      user.givenName = (String) payload.get("given_name");
+      user.sub = (String) payload.get("sub");
 
-    String id = addNewUser(user);
-    ctx.status(201);
-    ctx.json(ImmutableMap.of("id", id));
-
-  } else {
-    throw new BadRequestResponse("The requested user id wasn't a legal Mongo Object ID.");
+      String id = addNewUser(user);
+      ctx.status(201);
+      ctx.json(ImmutableMap.of("id", id));
+    }
+  }
+  else {
+    throw new BadRequestResponse("The requested user token was not legal.");
   }
 }
 }
