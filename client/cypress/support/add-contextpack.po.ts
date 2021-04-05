6e9e65a2acd1c9ecd401095da4785f531ba6a661
Thomas <dahlg136@morris.umn.edu>
2021-04-05 13:39:50 -0500

    Refactor googlelogin code for better code hub

--- client/cypress/support/add-contextpack.po.ts
+++ client/cypress/support/add-contextpack.po.ts
@@ -59,46 +59,29 @@ export class AddPackPage {
   }
 
   googleLogin(){
-    cy.log('Logging in to Google');
   cy.request({
     method: 'POST',
     url: 'https://www.googleapis.com/oauth2/v4/token',
     body: {
       grant_type: 'refresh_token',
       client_id: '239479898228-jsa8kqtcnqg96v8r74j2mp9jbbp01scu.apps.googleusercontent.com',
-      client_secret: 'fYWuoPB8RyEX4FWUxVhPFdUI',
-      refresh_token: '1//04kdq6BjVCbofCgYIARAAGAQSNwF-L9Ir5KLmutY2ti0WeoJqeRt_924wi3JuxOjJPvmkH6NlR0yUYo7wyNizniJQVywHMbgLJkE',
+      client_secret: process.env.API_SECRET,
+      refresh_token: '1//04PtCRYlxcBhaCgYIARAAGAQSNwF-L9IreHQjyhF-I-dhSUXxeumuvI8gkohBwRfSPp7f_PxGL-TvHaKU7zTF6vlUXpz5DaMYK68',
     },
   }).then(({ body }) => {
     const { access_token, id_token } = body;
-
     cy.request({
       method: 'GET',
       url: 'https://www.googleapis.com/oauth2/v3/userinfo',
       headers: { Authorization: `Bearer ${access_token}` },
     // eslint-disable-next-line no-shadow
     }).then(({ body }) => {
-      cy.log(body);
-      const userItem = {
-        token: id_token,
-        user: {
-          googleId: body.sub,
-          email: body.email,
-          email_verified: body.email_verified,
-          givenName: body.given_name,
-          familyName: body.family_name,
-          imageUrl: body.picture,
-        },
-      };
-
       cy.request('POST', 'api/users', id_token ).then(
   (response) => {
-
   }
 );
       this.navigateTo();
     });
   });
-
   }
 }
