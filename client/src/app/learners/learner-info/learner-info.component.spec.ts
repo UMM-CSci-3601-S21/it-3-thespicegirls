33611b9c5756ee49f66ed29ff9cd96baf79032ad
Cassie <schu4276@morris.umn.edu>
2021-04-18 15:13:45 -0500

    Route id from URL to info component
    This allows the learner id to be pulled from the URL.

--- client/src/app/learners/learner-info/learner-info.component.spec.ts
+++ client/src/app/learners/learner-info/learner-info.component.spec.ts
@@ -1,14 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRouteStub } from 'src/testing/activated-route-stub';
+import { MockLearnerService } from 'src/testing/learner.service.mock';
+import { LearnerService } from '../learner.service';
 
 import { LearnerInfoComponent } from './learner-info.component';
 
 describe('LearnerInfoComponent', () => {
   let component: LearnerInfoComponent;
   let fixture: ComponentFixture<LearnerInfoComponent>;
+  const activatedRoute: ActivatedRouteStub = new ActivatedRouteStub();
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LearnerInfoComponent ]
+      imports: [
+        RouterTestingModule,
+        MatCardModule,
+        MatSnackBarModule,
+        FormsModule,
+        ReactiveFormsModule,
+      ],
+      declarations: [ LearnerInfoComponent ],
+      providers: [
+        { provide: LearnerService, useValue: new MockLearnerService() },
+        { provide: ActivatedRoute, useValue: activatedRoute }
+      ]
     })
     .compileComponents();
   });
@@ -16,6 +36,13 @@ describe('LearnerInfoComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LearnerInfoComponent);
     component = fixture.componentInstance;
+    component.learner = {
+      _id: 'learner',
+      creator: 'string',
+      name: 'string',
+      assignedContextPacks: ['chris_id','chris_id'],
+      disabledWordlists: []
+    };
     fixture.detectChanges();
   });
 
--- client/src/app/learners/learner-info/learner-info.component.ts
+++ client/src/app/learners/learner-info/learner-info.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ContextPackService } from 'src/app/contextpacks/contextpack.service';
+import { Learner } from '../learner';
+import { LearnerService } from '../learner.service';
 
 @Component({
   selector: 'app-learner-info',
@@ -7,9 +13,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LearnerInfoComponent implements OnInit {
 
-  constructor() { }
+  learner: Learner;
+  id: string;
+  getLearnerSub: Subscription;
+  constructor(private route: ActivatedRoute,
+    private learnerService: LearnerService, private router: Router) { }
 
   ngOnInit(): void {
+    // We subscribe to the parameter map here so we'll be notified whenever
+    // that changes (i.e., when the URL changes) so this component will update
+    // to display the newly requested contextpack.
+    this.route.paramMap.subscribe((pmap) => {
+      this.id = pmap.get('id');
+      if (this.getLearnerSub) {
+        this.getLearnerSub.unsubscribe();
+      }
+      this.getLearnerSub = this.learnerService.getLearnerById(this.id).subscribe(learner => this.learner = learner);
+    });
   }
 
 }
