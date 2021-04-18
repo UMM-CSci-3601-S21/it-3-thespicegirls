e28df6211e19d817e144d2c41e2bb023843055c1
Cassie <schu4276@morris.umn.edu>
2021-04-17 22:11:59 -0500

    Set up Learner List tests
    This sets up all the Before each business that is needed to make the 'this component actually exists test' pass

--- client/src/app/learners/learner-card/learner-card.component.spec.ts
+++ client/src/app/learners/learner-card/learner-card.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { HttpTestingController } from '@angular/common/http/testing';
+import { ngfactoryFilePath } from '@angular/compiler/src/aot/util';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
@@ -8,6 +9,7 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ContextPack, Word, Wordlist } from 'src/app/contextpacks/contextpack';
 import { ContextPackService } from 'src/app/contextpacks/contextpack.service';
 import { ActivatedRouteStub } from 'src/testing/activated-route-stub';
 import { MockContextPackService } from 'src/testing/contextpack.service.mock';
@@ -33,7 +35,7 @@ describe('LearnerCardComponent', () => {
         FormsModule,
         ReactiveFormsModule,
       ],
-      declarations: [LearnerListComponent,],
+      declarations: [LearnerListComponent, LearnerCardComponent],
       providers: [
         {provide: LearnerService, useValue: new MockLearnerService()},
         { provide: ContextPackService, useValue: new MockContextPackService() }
@@ -46,6 +48,19 @@ describe('LearnerCardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LearnerCardComponent);
     component = fixture.componentInstance;
+    component.learner = {
+      _id: 'learner',
+      creator: 'string',
+      name: 'string',
+      assignedContextPacks: ['chris_id','chris_id'],
+      disabledWordlists: []
+    };
+    fixture.detectChanges();
+  });
+  beforeEach(() => {
+
+
+
     fixture.detectChanges();
   });
 
