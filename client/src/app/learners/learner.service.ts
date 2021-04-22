bf5bbc652755e31cb1bce56b6ffdc5baf53cba09
Natasha Zebrev <zebre001@morris.umn.edu>
2021-04-22 13:36:23 -0500

    Fix the addLearner method in learner.service to make the name required
    Updates the addLearner method so that an empty string is not accepted as a learner name, and the user cannot add a learner with an empty name.

--- client/src/app/learners/learner.service.ts
+++ client/src/app/learners/learner.service.ts
@@ -70,7 +70,10 @@ export class LearnerService {
   }
 
   addLearner(newLearner: Learner): Observable<string>{
-    return this.httpClient.post<{id: string}>(this.learnerUrl, newLearner).pipe(map(res => res.id));
+    if (newLearner.name.length > 0)
+    {return this.httpClient.post<{id: string}>(this.learnerUrl, newLearner).pipe(map(res => res.id));}
+    else
+    {alert('Unable to add learner with an empty name'); };
   }
 
 }
