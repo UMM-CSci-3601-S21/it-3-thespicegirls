e089f929e28e6fb6baa3c013539fa252b08ad147
Isabelle Hjelden <hjeld013@morris.umn.edu>
2021-04-27 11:31:52 -0800

    Create method for making a learner json file
    Created a method that takes all the assigned context packs from a learner and places them on to a single JSON file.
    Co-authored-by: JadynSondrol <JadynSondrol@users.noreply.github.com>

--- client/src/app/learners/learner-card/learner-card.component.ts
+++ client/src/app/learners/learner-card/learner-card.component.ts
@@ -2,6 +2,9 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Learner } from '../learner';
 import { ContextPack } from '../../contextpacks/contextpack';
 import { ContextPackService } from 'src/app/contextpacks/contextpack.service';
+import * as JSZip from 'jszip';
+import * as FileSaver from 'file-saver';
+import { threadId } from 'worker_threads';
 
 @Component({
   selector: 'app-learner-card',
@@ -27,7 +30,7 @@ export class LearnerCardComponent implements OnInit {
     // eslint-disable-next-line @typescript-eslint/prefer-for-of
     for(let i=0; i<this.learner.assignedContextPacks.length; i++){
      this.contextPackService.getContextPackById(this.learner.assignedContextPacks[i])
-      .subscribe(contextpack => this.assignedPacks.push(contextpack));
+      .subscribe(contextpack => {this.assignedPacks.push(contextpack);});
     }
   }
 
@@ -37,5 +40,40 @@ export class LearnerCardComponent implements OnInit {
       this.contextPackService.downloadJson(packs[i], packs[i].name).click();
     }
   }
+
+  downloadLearner(){
+    this.contextPackService.downloadLearnerJson(this.assignedPacks, this.learner.name).click();
+  }
+
+  // downloadZip(packs: ContextPack[]){
+  //   let i=0;
+  //   const newZip = new JSZip();
+  //   for(i; i<packs.length; i++){
+  //     newZip.file(packs[i].name, JSON.stringify[packs[i],2], {base64: true});
+  //   }
+
+  //   // this.contextPackService.downloadJson(zip.file, this.learner.name).click();
+  //   newZip.generateAsync ({ type: 'blob' })
+  //   .then(function(content) {
+  //     FileSaver.saveAs(content, this.learner.name);
+  //   });
+
+
+  // }
+
+
+  // downloadAZip(){
+  //   const zip = new JSZip();
+  //   zip.file('hello.txt', 'Hello World\n');
+
+  //   zip.generateAsync({ type: 'blob'}).then(function(content) {
+  //     FileSaver.saveAs(content, this.learner.name);
+  //   });
+  // }
+  // downloadZip1(){
+  //   const zip = new JSZip();
+  //   zip.file(this.learner.name, this.downloadAll(this.assignedPacks));
+  // }
+
 
 }
