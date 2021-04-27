d7085913d3b62ebea849f253981d0393e95ea7b4
Isabelle Hjelden <hjeld013@morris.umn.edu>
2021-04-27 11:58:24 -0800

    Clean up
    Clean up the mess of failed methods in the learner card typescript, also removed the previous method we were using before, and its HTML.
    
    Co-authored-by: JadynSondrol <JadynSondrol@users.noreply.github.com>

--- client/src/app/learners/learner-card/learner-card.component.ts
+++ client/src/app/learners/learner-card/learner-card.component.ts
@@ -2,9 +2,6 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Learner } from '../learner';
 import { ContextPack } from '../../contextpacks/contextpack';
 import { ContextPackService } from 'src/app/contextpacks/contextpack.service';
-import * as JSZip from 'jszip';
-import * as FileSaver from 'file-saver';
-import { threadId } from 'worker_threads';
 
 @Component({
   selector: 'app-learner-card',
@@ -34,46 +31,7 @@ export class LearnerCardComponent implements OnInit {
     }
   }
 
-  downloadAll(packs: ContextPack[]){
-    let i=0;
-    for(i; i<packs.length; i++){
-      this.contextPackService.downloadJson(packs[i], packs[i].name).click();
-    }
-  }
-
   downloadLearner(){
     this.contextPackService.downloadLearnerJson(this.assignedPacks, this.learner.name).click();
   }
-
-  // downloadZip(packs: ContextPack[]){
-  //   let i=0;
-  //   const newZip = new JSZip();
-  //   for(i; i<packs.length; i++){
-  //     newZip.file(packs[i].name, JSON.stringify[packs[i],2], {base64: true});
-  //   }
-
-  //   // this.contextPackService.downloadJson(zip.file, this.learner.name).click();
-  //   newZip.generateAsync ({ type: 'blob' })
-  //   .then(function(content) {
-  //     FileSaver.saveAs(content, this.learner.name);
-  //   });
-
-
-  // }
-
-
-  // downloadAZip(){
-  //   const zip = new JSZip();
-  //   zip.file('hello.txt', 'Hello World\n');
-
-  //   zip.generateAsync({ type: 'blob'}).then(function(content) {
-  //     FileSaver.saveAs(content, this.learner.name);
-  //   });
-  // }
-  // downloadZip1(){
-  //   const zip = new JSZip();
-  //   zip.file(this.learner.name, this.downloadAll(this.assignedPacks));
-  // }
-
-
 }
